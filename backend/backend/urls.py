33f78850d224baabf6dacaad16fac8c6e418fd3c
dhruvkher <dhruvkher77@gmail.com>
2025-12-19 16:31:52 +0530

    Pushing my Django LMS project changes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,34 +1,40 @@
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Employee Management API",
+        default_version='v1',
+        description="Employee, User, Manager APIs",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="support@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
+
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
 ]
 
-# from django.contrib import admin
-# from django.urls import path, include
-
-# from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
-
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="API Documentation",
-#         default_version='v1',
-#         description="LMS Backend API",
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-
-#     path('api/user/', include('user.urls')),
-
-#     # Swagger URLs
-#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-#     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-# ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
